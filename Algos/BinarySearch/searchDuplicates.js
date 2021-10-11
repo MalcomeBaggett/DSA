dbc51d1836ea0ee87db2b5565a54e1887eeb4875
Malcome Baggett <malcomebaggett@gmail.com>
2021-10-10 21:13:02 -0400

    added another binary search problem

---
+++ Algos/BinarySearch/searchDuplicates.js
@@ -0,0 +1,17 @@
+const findFirstOccurrence = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let boundary = -1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      boundary = mid;
+      right = mid - 1;
+    } else if (arr[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return boundary;
+};
